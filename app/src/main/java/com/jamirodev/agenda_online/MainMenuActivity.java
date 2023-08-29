19cd5406cec87736ed3c67d3ed56a3d1df48b088
Jamiro <jamirocristian15@gmail.com>
2023-08-29 13:26:30 -0400

    Carga de datos desde firebase: Realizado. Diseno simple para los datos: Realizado. Lo siguiente sera disenar y crear el menu principal para mostrar y organizar la data para mejorar la UI

--- app/src/main/java/com/jamirodev/agenda_online/MainMenuActivity.java
+++ app/src/main/java/com/jamirodev/agenda_online/MainMenuActivity.java
@@ -1,32 +1,55 @@
 package com.jamirodev.agenda_online;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainMenuActivity extends AppCompatActivity {
 
-    Button SignOff;
+    Button SignOut;
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
+
+    TextView NamesMain, MailMain;
+    ProgressBar ProgressBarData;
+
+    DatabaseReference Users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        SignOff = findViewById(R.id.SignOff);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Online schedule");
+
+        NamesMain = findViewById(R.id.NamesMain);
+        MailMain = findViewById(R.id.MailMain);
+        ProgressBarData = findViewById(R.id.ProgressBarData);
+
+        Users = FirebaseDatabase.getInstance().getReference("Users");
+
+        SignOut = findViewById(R.id.SignOut);
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
 
-        SignOff.setOnClickListener(new View.OnClickListener() {
+        SignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ExitApp();
@@ -34,6 +57,53 @@ public class MainMenuActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    protected void onStart() {
+        CheckLogin();
+        super.onStart();
+    }
+
+    private void CheckLogin() {
+        if (user!= null){
+            DataLoad();
+        }else {
+            startActivity(new Intent(MainMenuActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void DataLoad() {
+        Users.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                //if user exist
+                if (snapshot.exists()) {
+                    //hide progressbar
+                    ProgressBarData.setVisibility(View.GONE);
+                    //SHOWING TEXTVIEW
+                    NamesMain.setVisibility(View.VISIBLE);
+                    MailMain.setVisibility(View.VISIBLE);
+
+                    //GET DATA
+                    String names = ""+snapshot.child("Names").getValue();
+                    String mail = ""+snapshot.child("Gmail").getValue();
+
+                    //SET DATA ON TEXTVIEW
+                    NamesMain.setText(names);
+                    MailMain.setText(mail);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
     private void ExitApp() {
         firebaseAuth.signOut();
         startActivity(new Intent(MainMenuActivity.this, MainActivity.class));
