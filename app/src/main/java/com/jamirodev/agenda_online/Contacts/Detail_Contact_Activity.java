a60b8040c279b284afea4e645bd300f5e09baf5d
Jamiro <jamirocristian15@gmail.com>
2023-09-06 19:48:39 -0300

    Se han concedido los permisos para poder acceder al telefono y poder llamar a los contactos desde el manifest. Ahora tambien el usuario puede llamar a un contacto de su lista, si el contacto no tiene numero telefonico se ejecuta un Toast con el mensaje "theres no phone to call". El emulador no puede ejecutar las llamadas y no tengo como hacer la prueba real, pero segun el logcat se esta ejecutando correctamente. De por si tener en cuenta este commit ya que no es 100% seguro el llamado al no haber las sufucientes pruebas.

--- app/src/main/java/com/jamirodev/agenda_online/Contacts/Detail_Contact_Activity.java
+++ app/src/main/java/com/jamirodev/agenda_online/Contacts/Detail_Contact_Activity.java
@@ -1,8 +1,15 @@
 package com.jamirodev.agenda_online.Contacts;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,7 +46,12 @@ public class Detail_Contact_Activity extends AppCompatActivity {
         Call_C.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(Detail_Contact_Activity.this, "Call contact", Toast.LENGTH_SHORT).show();
+                if (ContextCompat.checkSelfPermission(Detail_Contact_Activity.this,
+                        Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    CallContact();
+                }else{
+                    CallPermissionRequest.launch(Manifest.permission.CALL_PHONE);
+                }
             }
         });
 
@@ -86,7 +98,7 @@ public class Detail_Contact_Activity extends AppCompatActivity {
         Name_Contact_D.setText("Name: "+name_c);
         Lastname_Contact_D.setText("Last Name: "+lastname_c);
         Mail_Contact_D.setText("Mail: "+mail_c);
-        Phone_Contact_D.setText("Phone: "+phone_c);
+        Phone_Contact_D.setText(phone_c);
         Age_Contact_D.setText("Age: "+age_c);
         Home_Contact_D.setText("Home: "+home_c);
     }
@@ -103,6 +115,26 @@ public class Detail_Contact_Activity extends AppCompatActivity {
 
         }
     }
+
+    private void CallContact() {
+        String phone = Phone_Contact_D.getText().toString();
+        if (!phone.equals("")){
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:"+phone));
+            startActivity(intent);
+        }else {
+            Toast.makeText(this, "There's no phone to call", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private ActivityResultLauncher<String> CallPermissionRequest =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted ->{
+                if (isGranted){
+                    CallContact();
+                }else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            });
 
     @Override
     public boolean onSupportNavigateUp() {
