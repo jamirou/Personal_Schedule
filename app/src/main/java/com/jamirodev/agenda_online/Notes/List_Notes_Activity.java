90cf1f9a0a6037fcaae7fd9cef8a7f4f712543cd
Jamiro <jamirocristian15@gmail.com>
2023-09-07 10:13:34 -0300

    Filtrado de notas terminado. He reutilizado el codigo de ListNotes para crear estados que posteriormente filtraran las notas. Tambien he incluido SharedPreferences a la activity List_Notes para guardar las preferencias del usuario al filtrar sus notas.

--- app/src/main/java/com/jamirodev/agenda_online/Notes/List_Notes_Activity.java
+++ app/src/main/java/com/jamirodev/agenda_online/Notes/List_Notes_Activity.java
@@ -10,6 +10,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -52,6 +53,8 @@ public class List_Notes_Activity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser user;
 
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,12 +77,239 @@ public class List_Notes_Activity extends AppCompatActivity {
         firebaseDatabase = FirebaseDatabase.getInstance();
         DB_Users = firebaseDatabase.getReference("Users");
         dialog = new Dialog(List_Notes_Activity.this);
-        ListNotesUsers();
+        FilterStatus();
+
     }
 
-    private void ListNotesUsers() {
+    private void ListAllNotesUsers() {
         //CONSULT
         Query query = DB_Users.child(user.getUid()).child("Published notes").orderByChild("date_note");
+        options = new FirebaseRecyclerOptions.Builder<Note>().setQuery(query, Note.class).build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Note, ViewHolder_Note>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolder_Note viewHolder_note, int position, @NonNull Note note) {
+                viewHolder_note.SetData(
+                        getApplicationContext(),
+                        note.getId_note(),
+                        note.getUid_user(),
+                        note.getMail_user(),
+                        note.getDate_actual_hour(),
+                        note.getTitle(),
+                        note.getDescription(),
+                        note.getDate_note(),
+                        note.getState()
+                );
+            }
+
+            @NonNull
+            @Override
+            public ViewHolder_Note onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_note, parent, false);
+                ViewHolder_Note viewHolder_note = new ViewHolder_Note(view);
+                viewHolder_note.setOnClickListener(new ViewHolder_Note.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        String id_note = getItem(position).getId_note();
+                        String uid_user = getItem(position).getUid_user();
+                        String mail_user = getItem(position).getMail_user();
+                        String date_register = getItem(position).getDate_actual_hour();
+                        String title = getItem(position).getTitle();
+                        String description = getItem(position).getDescription();
+                        String date_note = getItem(position).getDate_note();
+                        String state = getItem(position).getState();
+
+                        //SEND DATA TO DETAIL ACTIVITY
+                        Intent intent = new Intent(List_Notes_Activity.this, Detail_Note_Activity.class);
+                        intent.putExtra("id_note", id_note);
+                        intent.putExtra("uid_user", uid_user);
+                        intent.putExtra("mail_user", mail_user);
+                        intent.putExtra("date_register", date_register);
+                        intent.putExtra("title", title);
+                        intent.putExtra("description", description);
+                        intent.putExtra("date_note", date_note);
+                        intent.putExtra("state", state);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+
+//                        GET SELECTED NOTE DATA
+                        String id_note = getItem(position).getId_note();
+                        String uid_user = getItem(position).getUid_user();
+                        String mail_user = getItem(position).getMail_user();
+                        String date_register = getItem(position).getDate_actual_hour();
+                        String title = getItem(position).getTitle();
+                        String description = getItem(position).getDescription();
+                        String date_note = getItem(position).getDate_note();
+                        String state = getItem(position).getState();
+
+
+//                        DECLARE VIEWS
+                        Button CD_Delete, CD_Update;
+
+//                        CONNECT VIEWS WITH DESIGN
+                        dialog.setContentView(R.layout.dialog_options);
+                        CD_Delete = dialog.findViewById(R.id.CD_Delete);
+                        CD_Update = dialog.findViewById(R.id.CD_Update);
+
+                        CD_Delete.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                DeleteNote(id_note);
+                                dialog.dismiss();
+                            }
+                        });
+                        CD_Update.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+//                                Toast.makeText(List_Notes_Activity.this, "Update note", Toast.LENGTH_SHORT).show();
+//                                startActivity(new Intent(List_Notes_Activity.this, Update_Note_Activity.class));
+                                Intent intent = new Intent(List_Notes_Activity.this, Update_Note_Activity.class);
+                                intent.putExtra("id_note", id_note);
+                                intent.putExtra("uid_user", uid_user);
+                                intent.putExtra("mail_user", mail_user);
+                                intent.putExtra("date_register", date_register);
+                                intent.putExtra("title", title);
+                                intent.putExtra("description", description);
+                                intent.putExtra("date_note", date_note);
+                                intent.putExtra("state", state);
+                                startActivity(intent);
+                                dialog.dismiss();
+                            }
+                        });
+                        dialog.show();
+                    }
+                });
+                return viewHolder_note;
+            }
+        };
+
+        linearLayoutManager = new LinearLayoutManager(List_Notes_Activity.this, LinearLayoutManager.VERTICAL, false);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        rvNotes.setLayoutManager(linearLayoutManager);
+        rvNotes.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    private void ListFinishedNotes() {
+        //CONSULT
+        String state_note = "finished";
+        Query query = DB_Users.child(user.getUid()).child("Published notes").orderByChild("state").equalTo(state_note);
+        options = new FirebaseRecyclerOptions.Builder<Note>().setQuery(query, Note.class).build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Note, ViewHolder_Note>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolder_Note viewHolder_note, int position, @NonNull Note note) {
+                viewHolder_note.SetData(
+                        getApplicationContext(),
+                        note.getId_note(),
+                        note.getUid_user(),
+                        note.getMail_user(),
+                        note.getDate_actual_hour(),
+                        note.getTitle(),
+                        note.getDescription(),
+                        note.getDate_note(),
+                        note.getState()
+                );
+            }
+
+            @NonNull
+            @Override
+            public ViewHolder_Note onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_note, parent, false);
+                ViewHolder_Note viewHolder_note = new ViewHolder_Note(view);
+                viewHolder_note.setOnClickListener(new ViewHolder_Note.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        String id_note = getItem(position).getId_note();
+                        String uid_user = getItem(position).getUid_user();
+                        String mail_user = getItem(position).getMail_user();
+                        String date_register = getItem(position).getDate_actual_hour();
+                        String title = getItem(position).getTitle();
+                        String description = getItem(position).getDescription();
+                        String date_note = getItem(position).getDate_note();
+                        String state = getItem(position).getState();
+
+                        //SEND DATA TO DETAIL ACTIVITY
+                        Intent intent = new Intent(List_Notes_Activity.this, Detail_Note_Activity.class);
+                        intent.putExtra("id_note", id_note);
+                        intent.putExtra("uid_user", uid_user);
+                        intent.putExtra("mail_user", mail_user);
+                        intent.putExtra("date_register", date_register);
+                        intent.putExtra("title", title);
+                        intent.putExtra("description", description);
+                        intent.putExtra("date_note", date_note);
+                        intent.putExtra("state", state);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+
+//                        GET SELECTED NOTE DATA
+                        String id_note = getItem(position).getId_note();
+                        String uid_user = getItem(position).getUid_user();
+                        String mail_user = getItem(position).getMail_user();
+                        String date_register = getItem(position).getDate_actual_hour();
+                        String title = getItem(position).getTitle();
+                        String description = getItem(position).getDescription();
+                        String date_note = getItem(position).getDate_note();
+                        String state = getItem(position).getState();
+
+
+//                        DECLARE VIEWS
+                        Button CD_Delete, CD_Update;
+
+//                        CONNECT VIEWS WITH DESIGN
+                        dialog.setContentView(R.layout.dialog_options);
+                        CD_Delete = dialog.findViewById(R.id.CD_Delete);
+                        CD_Update = dialog.findViewById(R.id.CD_Update);
+
+                        CD_Delete.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                DeleteNote(id_note);
+                                dialog.dismiss();
+                            }
+                        });
+                        CD_Update.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+//                                Toast.makeText(List_Notes_Activity.this, "Update note", Toast.LENGTH_SHORT).show();
+//                                startActivity(new Intent(List_Notes_Activity.this, Update_Note_Activity.class));
+                                Intent intent = new Intent(List_Notes_Activity.this, Update_Note_Activity.class);
+                                intent.putExtra("id_note", id_note);
+                                intent.putExtra("uid_user", uid_user);
+                                intent.putExtra("mail_user", mail_user);
+                                intent.putExtra("date_register", date_register);
+                                intent.putExtra("title", title);
+                                intent.putExtra("description", description);
+                                intent.putExtra("date_note", date_note);
+                                intent.putExtra("state", state);
+                                startActivity(intent);
+                                dialog.dismiss();
+                            }
+                        });
+                        dialog.show();
+                    }
+                });
+                return viewHolder_note;
+            }
+        };
+
+        linearLayoutManager = new LinearLayoutManager(List_Notes_Activity.this, LinearLayoutManager.VERTICAL, false);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        rvNotes.setLayoutManager(linearLayoutManager);
+        rvNotes.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    private void ListNotesNotFinished() {
+        //CONSULT
+        String state_note = "Not finished";
+        Query query = DB_Users.child(user.getUid()).child("Published notes").orderByChild("state").equalTo(state_note);
         options = new FirebaseRecyclerOptions.Builder<Note>().setQuery(query, Note.class).build();
         firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Note, ViewHolder_Note>(options) {
             @Override
@@ -306,6 +536,10 @@ public class List_Notes_Activity extends AppCompatActivity {
         All_Notes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("List", "All");
+                editor.apply();
+                recreate();
                 Toast.makeText(List_Notes_Activity.this, "All notes", Toast.LENGTH_SHORT).show();
                 dialog_filter.dismiss();
             }
@@ -314,7 +548,11 @@ public class List_Notes_Activity extends AppCompatActivity {
         Finished_notes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(List_Notes_Activity.this, "Finished notes", Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("List", "finished");
+                editor.apply();
+                recreate();
+                Toast.makeText(List_Notes_Activity.this, "finished notes", Toast.LENGTH_SHORT).show();
                 dialog_filter.dismiss();
             }
         });
@@ -322,6 +560,10 @@ public class List_Notes_Activity extends AppCompatActivity {
         Notes_NotFinished.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("List", "Not finished");
+                editor.apply();
+                recreate();
                 Toast.makeText(List_Notes_Activity.this, "Notes not finished", Toast.LENGTH_SHORT).show();
                 dialog_filter.dismiss();
             }
@@ -329,6 +571,24 @@ public class List_Notes_Activity extends AppCompatActivity {
 
         dialog_filter.show();
 
+    }
+
+    private void FilterStatus() {
+        sharedPreferences = List_Notes_Activity.this.getSharedPreferences("Notes", MODE_PRIVATE);
+
+        String filter_status = sharedPreferences.getString("List", "All");
+
+        switch (filter_status){
+            case "All":
+                ListAllNotesUsers();
+                break;
+            case  "finished":
+                ListFinishedNotes();
+                break;
+            case  "Not finished":
+                ListNotesNotFinished();
+                break;
+        }
     }
 
     @Override
